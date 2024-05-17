7f5c3b1800e084ab0fb95b1e9accde7db2dbab9b
Golembr27 <142447072+Golembr27@users.noreply.github.com>
2024-05-17 14:27:07 -0300

    V 0.2
    1- Terminei todo o menu

--- Assets/Scripts/IniciarOjogo.cs
+++ Assets/Scripts/IniciarOjogo.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IniciarOjogo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    
+    string nomeCena;
+    public string cenaAtual;
+
+    void Start(){
+        cenaAtual = nomeCena;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void TrocarDeCena(){
+        SceneManager.LoadScene(cenaAtual);
     }
+
 }
